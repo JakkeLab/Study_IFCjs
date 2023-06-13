ba04cf242c324be4366708302e87fdd0c64a8bff
JakkeLab <itsjakk3@gmail.com>
2023-06-13 21:41:58 +0900

    First Web-ifc-viewer run

---
+++ 02_HelloWorldWIV/app.js
@@ -0,0 +1,15 @@
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({container, backgroundColor : new Color(0xffffff)});
+viewer.axes.setAxes();
+viewer.grid.setGrid();
+
+load();
+
+async function load() {
+    const model = await viewer.IFC.loadIfcUrl('./01.ifc');
+    await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
+}
